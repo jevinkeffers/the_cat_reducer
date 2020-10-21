6ce9d7dc7735b2a136fbb7b7a5bae19f82bf7a0d
Kevin Jeffers <jevinkeffers@gmail.com>
2020-10-21 11:04:17 -0400

    Setting up Redux boilerplate

---
+++ src/components/Activity.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Activity = () => (
+    <>
+        <h1>Whoa a kitty. What's it doing?</h1>
+        <h2>React with Redux Activity Center</h2>
+    </>
+);
+
+export default Activity;
