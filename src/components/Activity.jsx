9254d52dbd29d231ca569aaa0a9dfbccda960b4b
Kevin Jeffers <jevinkeffers@gmail.com>
2020-10-21 15:42:31 -0400

    Exercise 2 changes

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,30 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
-import { eat, play, nap } from "../redux/actions";
+import { eat, play, nap, setName } from "../redux/actions";
 
-const Activity = ({ activity, eat, play, nap }) => (
-    <>
-        <h1>Whoa a kitty. What's it doing?</h1>
-        <h2>React with Redux Activity Center</h2>
-        <p>The cat is {activity}</p>
-        <button type="button" onClick={()=>eat()}>
-            Eating
-        </button>
-        <button type="button" onClick={()=>play()}>
-            Playing
-        </button>
-        <button type="button" onClick={()=>nap()}>
-            Napping
-        </button>
-        <p>
-        <img src="http://24.media.tumblr.com/ea04a79500ff96505e3d30ece2a31580/tumblr_mhjhn6ex8A1rum37jo1_400.gif"></img>
-        </p>
-    </>
-);
+const Activity = ({ name, activity, eat, play, nap, setName }) => {
+    const [newName, setNewName] = useState("");
 
-const mapStateToProps = state => {
-    const { activity } = state;
-    return activity;
-}
+    const _handleChange = (name) => {
+        setNewName(name);
+    };
+    const _handleClick = () => {
+        setName(newName);
+    };
+    return (
+        <>
+            <h1>Whoa a kitty. What's it doing?</h1>
+            <h2>React with Redux Activity Center</h2>
+            <p>{name} is {activity}</p>
+            <form>
+                <label>
+                Cat's Name
+                <input
+                    type="text"
+                    onChange={(e) => _handleChange(e.target.value)}
+                    value={newName}
+                />
+                </label>
+                <button type="button" onClick={() => _handleClick()}>
+                Submit
+                </button>
+            <p></p>    
+            </form>
+            <button type="button" onClick={()=>eat()}>
+                Eating
+            </button>
+            <button type="button" onClick={()=>play()}>
+                Playing
+            </button>
+            <button type="button" onClick={()=>nap()}>
+                Napping
+            </button>
+            <p>
+            <img src="http://24.media.tumblr.com/ea04a79500ff96505e3d30ece2a31580/tumblr_mhjhn6ex8A1rum37jo1_400.gif" alt="kitty"></img>
+            </p>
+        </>
+    );
+};
 
-export default connect(mapStateToProps, { eat, play, nap })(Activity);
+
+const mapStateToProps = (state) => {
+    const { name, activity } = state;
+    return { name, activity };
+};
+
+export default connect(mapStateToProps, { eat, play, nap, setName })(Activity);
---
+++ src/redux/reducers/name.js
@@ -0,0 +1,16 @@
+import { ACTION_SET_NAME } from "../actionTypes";
+
+const initialState = "Dewey"
+
+const nameReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case ACTION_SET_NAME: {
+        const { name } = action.payload;
+        return name;
+        }
+        default:
+        return state;
+    }
+};
+
+export default nameReducer;
